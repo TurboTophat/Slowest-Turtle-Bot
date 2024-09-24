0d1e40c94dc58aa3de52ebc6d48aa70095b4756b
TurboTophat <103705461+TurboTophat@users.noreply.github.com>
2024-09-24 03:21:10 -0400

    testing the stream code with something new

--- cogs/StreamNotification.py
+++ cogs/StreamNotification.py
@@ -1,8 +1,8 @@
-import nextcord
+import os
+from apikeys import TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET
 from nextcord.ext import tasks, commands
 import aiohttp
 import json
-from apikeys import TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET
 
 class StreamNotification(commands.Cog):
     def __init__(self, bot):
@@ -10,11 +10,11 @@ class StreamNotification(commands.Cog):
         self.twitch_client_id = TWITCH_CLIENT_ID
         self.twitch_client_secret = TWITCH_CLIENT_SECRET
         self.auth_token = None
-        self.streamers = {}  # {discord_user_id: [twitch_username_1, twitch_username_2]}
-        self.live_streams = {}  # {twitch_username: live_status}
+        self.streamers = []  # List of Twitch usernames
+        self.live_streams = {}  # Dictionary to track live status {twitch_username: live_status}
         self.notification_channel_id = None
         self.load_channel_id()
-        self.check_stream.start()
+        self.check_streams.start()  # Start the task loop
 
     async def get_twitch_token(self):
         """Get an OAuth token for Twitch API."""
@@ -42,64 +42,60 @@ class StreamNotification(commands.Cog):
                 headers=headers
             ) as response:
                 data = await response.json()
-                if data['data']:  # Stream is live if there's data
-                    return True
-                return False
+                return bool(data['data'])  # Stream is live if there's data
 
     @commands.command(name='setlivechannel')
     async def set_channel(self, ctx, channel: commands.TextChannelConverter):
         """Sets the channel where live notifications will be sent."""
         self.notification_channel_id = channel.id
-        self.save_channel_id(channel.id)  # Save the channel ID to a file
+        self.save_channel_id(channel.id)
         await ctx.send(f"Live notifications will be sent to {channel.mention}")
 
     @commands.command(name='addstreamer')
     async def add_streamer(self, ctx, twitch_username: str):
         """Add a Twitch streamer to the watch list."""
-        if ctx.author.id not in self.streamers:
-            self.streamers[ctx.author.id] = []
-        if twitch_username not in self.streamers[ctx.author.id]:
-            self.streamers[ctx.author.id].append(twitch_username)
+        if twitch_username not in self.streamers:
+            self.streamers.append(twitch_username)
             self.live_streams[twitch_username] = False  # Assume not live initially
-            await ctx.send(f"Added {twitch_username} to your watch list!")
+            await ctx.send(f"Added {twitch_username} to the streamer watch list!")
         else:
-            await ctx.send(f"{twitch_username} is already on your list!")
+            await ctx.send(f"{twitch_username} is already on the watch list!")
 
     @commands.command(name='removestreamer')
     async def remove_streamer(self, ctx, twitch_username: str):
-        """Remove a Twitch streamer from the watch list by name."""
-        if ctx.author.id in self.streamers and twitch_username in self.streamers[ctx.author.id]:
-            self.streamers[ctx.author.id].remove(twitch_username)
-            # Optionally remove the streamer from the live_streams tracking
-            if twitch_username in self.live_streams:
-                del self.live_streams[twitch_username]
-            await ctx.send(f"Removed {twitch_username} from your watch list.")
+        """Remove a Twitch streamer from the watch list."""
+        if twitch_username in self.streamers:
+            self.streamers.remove(twitch_username)
+            del self.live_streams[twitch_username]  # Remove their live status tracking
+            await ctx.send(f"Removed {twitch_username} from the watch list.")
         else:
-            await ctx.send(f"{twitch_username} is not on your list.")
+            await ctx.send(f"{twitch_username} is not on the watch list.")
 
     @tasks.loop(minutes=5)  # Check every 5 minutes
-    async def check_stream(self):
-        """Periodically check if streamers are live."""
+    async def check_streams(self):
+        """Periodically check if streamers in the list are live."""
         if not self.auth_token:
             await self.get_twitch_token()
 
         # Loop through each registered streamer
-        for user_id, streamers in self.streamers.items():
-            for streamer_name in streamers:
-                is_live = await self.is_stream_live(streamer_name)
-                
-                # If the stream is live and wasn't already live
-                if is_live and not self.live_streams.get(streamer_name, False):
-                    self.live_streams[streamer_name] = True  # Mark as live
-                    # Send notification to the specified channel
-                    if self.notification_channel_id:
-                        channel = self.bot.get_channel(self.notification_channel_id)
-                        if channel:
-                            await channel.send(f"@everyone, {streamer_name} is now live! Watch here: https://www.twitch.tv/{streamer_name}")
+        for streamer_name in self.streamers:
+            is_live = await self.is_stream_live(streamer_name)
 
-                # If the stream is not live anymore, reset the live status
-                elif not is_live and self.live_streams.get(streamer_name, False):
-                    self.live_streams[streamer_name] = False  # Mark as not live
+            # If the stream is live and wasn't already marked as live
+            if is_live and not self.live_streams.get(streamer_name, False):
+                self.live_streams[streamer_name] = True  # Mark as live
+                await self.notify_live(streamer_name)
+
+            # If the stream is no longer live, reset live status
+            elif not is_live and self.live_streams.get(streamer_name, False):
+                self.live_streams[streamer_name] = False  # Mark as not live
+
+    async def notify_live(self, streamer_name):
+        """Notify the channel that a streamer has gone live."""
+        if self.notification_channel_id:
+            channel = self.bot.get_channel(self.notification_channel_id)
+            if channel:
+                await channel.send(f"@everyone, {streamer_name} is now live! Watch here: https://www.twitch.tv/{streamer_name}")
 
     # Save the notification channel ID to a file
     def save_channel_id(self, channel_id):
@@ -115,8 +111,8 @@ class StreamNotification(commands.Cog):
         except FileNotFoundError:
             self.notification_channel_id = None
 
-    @check_stream.before_loop
-    async def before_check_stream(self):
+    @check_streams.before_loop
+    async def before_check_streams(self):
         """Wait until the bot is ready before starting the task."""
         await self.bot.wait_until_ready()
 
